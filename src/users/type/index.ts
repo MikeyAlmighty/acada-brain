83297d53ca5be67c6f0254451093794d337ce096
Michael Leibbrandt <mike.leibbrandt@gmail.com>
2024-08-20 08:32:07 +0200

    Added UserDTO and params

---
+++ src/users/type/index.ts
@@ -0,0 +1,11 @@
+export type CreateUserParams = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+};
+
+export type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+};
